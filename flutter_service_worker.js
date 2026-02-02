28a758b5b9407d30bb9cb1963363c8feb84206bb
Mohammedmilad1969 <mohammedmilad1969@gmail.com>
2026-02-02 19:09:22 +0200

    Auto web deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "63492640fca276d6b8d04680ef74f48b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f9b48b32ba384d8340360482d366328a",
+"flutter_bootstrap.js": "7b84bc639c328f686db8ee94a2079213",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
